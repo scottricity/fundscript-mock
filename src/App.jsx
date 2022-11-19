13890c3112a9f46c9f38739fdd46f0e2a4dbec7d
Timothy3773 <jonathanscott122@gmail.com>
2022-11-19 00:16:42 -0500

    wow

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from "react";
+import {Box, Container, Button, Typography} from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Typography>lol</Typography>
+    </Box>
+  );
+}
+
+export default App;
